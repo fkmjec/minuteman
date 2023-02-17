ea3d6518d217ee9698843406d3bfa1d761376064
František Kmječ <frantisek.kmjec@gmail.com>
2023-02-17 17:33:41 +0100

    Display possible output

--- web_frontend/main.py
+++ web_frontend/main.py
@@ -8,4 +8,9 @@ app.config['SECRET_KEY'] = 'lkajflkejfnlaneom zo3r0194fnoaijl'
 @app.route("/", methods=["GET", "POST"])
 def index():
     form = TranscriptInputForm()
-    return render_template("index.html", title="Minuteman", form=form)
+
+    minutes = ""
+    if form.validate_on_submit():
+        # send to the model
+        minutes = form.transcript.data
+    return render_template("index.html", title="Minuteman", form=form, minutes=minutes)

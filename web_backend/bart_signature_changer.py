d2a165d84525a691772e7519c9b1362a392b2e1b
František Kmječ <frantisek.kmjec@gmail.com>
2023-03-05 08:05:07 +0100

    Fix a bug in the input signature of the BART model

--- web_backend/bart_signature_changer.py
+++ web_backend/bart_signature_changer.py
@@ -8,11 +8,19 @@ parser.add_argument("--from_path", type=str, help="where to load the model from"
 parser.add_argument("--save_to", type=str, help="where to save the resulting model for tf serving")
 
 # Creation of a subclass in order to define a new serving signature
-class ModifiedModel(TFAutoModelForSeq2SeqLM):
+class GenerationModel(TFAutoModelForSeq2SeqLM):
     # Decorate the serving method with the new input_signature
     # an input_signature represents the name, the data type and the shape of an expected input
+    # @tf.function(input_signature=[{
+    #     "input_ids": tf.TensorSpec((None, None), tf.int32, name="input_ids"),
+    # }])
+    # def generate_output(self, inputs):
+    #     # TODO: include tokenizer here?
+    #     output = self.generate(inputs["input_ids"])
+    #     return output
+    
     @tf.function(input_signature=[{
-        "inputs_ids": tf.TensorSpec((None, None), tf.int64, name="input_ids"),
+        "input_ids": tf.TensorSpec((None, None), tf.int32, name="input_ids"),
     }])
     def serving(self, inputs):
         # call the model to process the inputs
@@ -25,7 +33,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # Instantiate the model with the new serving method
     print("loading model...")
-    model = ModifiedModel.from_pretrained("facebook/bart-large-xsum")
+    model = GenerationModel.from_pretrained("facebook/bart-large-xsum")
     print("saving model with a different signature...")
-    model.save("bart-large-xsum", save_format="tf")
+    model.save_pretrained(args.save_to, saved_model=True)
 

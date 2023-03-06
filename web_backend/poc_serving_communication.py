d215af58862b233ad5e46bb0d2b36ee845882820
František Kmječ <frantisek.kmjec@gmail.com>
2023-03-06 12:45:43 +0100

    Modify communication to work with TorchServe

--- web_backend/poc_serving_communication.py
+++ web_backend/poc_serving_communication.py
@@ -1,18 +1,14 @@
 import requests
-import json
-import transformers
 
 MODEL_CHECKPOINT = "facebook/bart-large-xsum"
 
-tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_CHECKPOINT)
+# tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_CHECKPOINT)
 
-text = input("Please type in a bit of text to summarize")
-model_input = tokenizer(text)
-model_input = dict(model_input)
+model_input = input("Please type in a bit of text to summarize: ")
 
-rest_url = 'http://localhost:8501/v1/models/final_finetuned:predict'
-json_data = {'signature_name': 'serving_default', 'instances': [model_input]}
 
-json_response = requests.post(rest_url, json=json_data)
-pred = json.loads(json_response.text)
-print(pred)
+rest_url = 'http://localhost:8080/predictions/bart'
+files = {'model_input': ('model_input.txt', model_input)}
+response = requests.post(rest_url, files=files)
+
+print(response)

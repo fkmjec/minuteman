a9fabe477b9191a07a4fb83ba46d0819a3416914
František Kmječ <frantisek.kmjec@gmail.com>
2023-03-06 13:02:05 +0100

    Rename file to data

--- web_backend/poc_serving_communication.py
+++ web_backend/poc_serving_communication.py
@@ -8,7 +8,7 @@ model_input = input("Please type in a bit of text to summarize: ")
 
 
 rest_url = 'http://localhost:8080/predictions/bart'
-files = {'model_input': ('model_input.txt', model_input)}
+files = {'data': ('model_input.txt', model_input)}
 response = requests.post(rest_url, files=files)
 
 print(response)

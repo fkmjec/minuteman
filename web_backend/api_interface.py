50890c52b271090e86da096cb32bb957c7d7c8d0
František Kmječ <frantisek.kmjec@gmail.com>
2023-03-20 07:54:28 +0100

    Add reading of length of wav audio

--- web_backend/api_interface.py
+++ web_backend/api_interface.py
@@ -1,4 +1,6 @@
 import requests
+import wave
+import io
 
 # TODO: this belongs somewhere systematic
 TORCH_API_ADDRESS = "http://torchserve:8084/predictions/bart"
@@ -13,4 +15,10 @@ def summarize_block(input_string):
     return response.text
 
 def transcribe_chunk(chunk):
-    return "TODO: transcribe chunk"
+    # get length of chunk in seconds
+    audio = io.BytesIO(chunk)
+    with wave.open(audio) as wopen:
+        frames = wopen.getnframes()
+        rate = wopen.getframerate()
+        duration = frames / float(rate)
+    return "TODO: len of audio is {} seconds".format(duration)

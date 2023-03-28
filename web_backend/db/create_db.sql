718859c5dbeb9befa2d8652aa4bb28f5ba11969e
František Kmječ <frantisek.kmjec@gmail.com>
2023-03-28 10:10:36 +0200

    Datetime -> timestamp

--- web_backend/db/create_db.sql
+++ web_backend/db/create_db.sql
@@ -3,7 +3,7 @@ CREATE DATABASE minuteman;
 -- the session that is created after the user logs into the webpage
 CREATE TABLE minuteman_session (
   id varchar(20) not null,
-  creation_time datetime not null,
+  creation_time timestamp not null,
   primary key (id)
 );
 
@@ -12,7 +12,7 @@ CREATE TABLE transcribed_utterance (
   id integer not null,
   author varchar(100) not null,
   utterance varchar(1000) not null,
-  time datetime not null,
+  time timestamp not null,
   minuteman_session_id varchar(20) references minuteman_session(id),
   primary key (id)
 );

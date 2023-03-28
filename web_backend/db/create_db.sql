edb59bfbf58819e1cd87503245dca61368e57e73
František Kmječ <frantisek.kmjec@gmail.com>
2023-03-28 10:00:42 +0200

    Fix bad sql syntax

--- web_backend/db/create_db.sql
+++ web_backend/db/create_db.sql
@@ -1,17 +1,18 @@
 CREATE DATABASE minuteman;
 
 -- the session that is created after the user logs into the webpage
-CREATE TABLE minuteman_session {
-    id varchar(20) primary key not null,
-    creation_time datetime not null,
-};
+CREATE TABLE minuteman_session (
+  id varchar(20) not null,
+  creation_time datetime not null,
+  primary key (id)
+);
 
 -- the first utterances that were recorded from the meeting, without user editing
-CREATE TABLE transcribed_utterance {
-    id integer not null,
-    author varchar(100) not null,
-    utterance varchar(1000) not null,
-    time datetime not null,
-    minuteman_session_id varchar(20) foreign key references minuteman_session(id),
-    primary key id
-};
+CREATE TABLE transcribed_utterance (
+  id integer not null,
+  author varchar(100) not null,
+  utterance varchar(1000) not null,
+  time datetime not null,
+  minuteman_session_id varchar(20) references minuteman_session(id),
+  primary key (id)
+);

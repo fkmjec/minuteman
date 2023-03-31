2f0cfdcaaa3765798545308f6df0ba3ab5d79b5b
František Kmječ <frantisek.kmjec@gmail.com>
2023-03-31 17:09:42 +0200

    Change compose to work better on SLT

--- web_backend/config.py
+++ web_backend/config.py
@@ -6,6 +6,5 @@ class Config():
         self.mock_ml_models = os.environ["MOCK_ML_MODELS"] == "true"
         self.torch_backend_url = os.environ["TORCH_BACKEND_URL"]
         self.db_url = os.environ["POSTGRES_DB_URL"]
-        self.flask_app_url = os.environ["FLASK_APP_URL"]
         # TODO: from variables? by model?
         self.max_input_len = 512

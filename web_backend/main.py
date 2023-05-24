bdd6e2ed9292c36102818f932f1a067ba5eec583
František Kmječ <frantisek.kmjec@gmail.com>
2023-05-24 18:26:26 +0200

    Move transcription worker to its own container

--- web_backend/main.py
+++ web_backend/main.py
@@ -7,7 +7,7 @@ import etherpad_interface
 import view_utils
 import text_utils
 import config
-import audio_chunk
+from transcription_worker import audio_chunk
 from models import DBInterface
 from flask import Flask, jsonify, render_template, request, redirect, url_for, abort
 from forms import TranscriptInputForm
@@ -28,44 +28,44 @@ app.config['SQLALCHEMY_DATABASE_URI'] = app_config.db_url
 db.init_app(app)
 
 # initialize global interfaces with config from env vars
-torch_interface = api_interface.TorchInterface(app_config)
+# torch_interface = api_interface.TorchInterface(app_config)
 db_interface = DBInterface(app_config)
 editor_interface = etherpad_interface.PadInterface(app_config)
-tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-xsum")
+# tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-xsum")
 
 # FIXME: this should be more structured
 gunicorn_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers = gunicorn_logger.handlers
 
-def update_minutes(session_id, transcript, past_minutes):
-    splits = text_utils.split_by_trsc_separator(transcript)
-    last_split = splits[-1]
-    minutes = [torch_interface.summarize_block(split) for split in splits]
-    i = 0
-    chars_before = 0
-    for minute, split in zip(minutes[0:-1], splits[0:-1]):
-        # app.logger.info(split)
-        split_len = len(split)
-        line_id = session_id + "-" + str(i)
-        # FIXME: this is going to be slow
-        already_generated = False
-        for past_minute in past_minutes:
-            m_id = past_minute[0]
-            if m_id == line_id:
-                already_generated = True
-        if already_generated:
-            editor_interface.update_summ_line(session_id, minute, line_id)
-        else:
-            editor_interface.add_summ_line(session_id, minute, line_id, chars_before, chars_before + split_len + 1)
-        i += 1
-        chars_before += split_len + 1
+# def update_minutes(session_id, transcript, past_minutes):
+#     splits = text_utils.split_by_trsc_separator(transcript)
+#     last_split = splits[-1]
+#     minutes = [torch_interface.summarize_block(split) for split in splits]
+#     i = 0
+#     chars_before = 0
+#     for minute, split in zip(minutes[0:-1], splits[0:-1]):
+#         # app.logger.info(split)
+#         split_len = len(split)
+#         line_id = session_id + "-" + str(i)
+#         # FIXME: this is going to be slow
+#         already_generated = False
+#         for past_minute in past_minutes:
+#             m_id = past_minute[0]
+#             if m_id == line_id:
+#                 already_generated = True
+#         if already_generated:
+#             editor_interface.update_summ_line(session_id, minute, line_id)
+#         else:
+#             editor_interface.add_summ_line(session_id, minute, line_id, chars_before, chars_before + split_len + 1)
+#         i += 1
+#         chars_before += split_len + 1
     
-    # append new line if it has one full segment
-    last_splits = text_utils.split_to_lens(last_split, app_config.max_input_len, tokenizer)
-    if len(last_splits) > 1:
-        last_minute = torch_interface.summarize_block(last_splits[0])
-        last_line_id = session_id + "-" + str(i)
-        editor_interface.add_summ_line(session_id, last_minute, last_line_id, chars_before, chars_before + len(last_splits[0]) + 1)
+    # # append new line if it has one full segment
+    # last_splits = text_utils.split_to_lens(last_split, app_config.max_input_len, tokenizer)
+    # if len(last_splits) > 1:
+    #     last_minute = torch_interface.summarize_block(last_splits[0])
+    #     last_line_id = session_id + "-" + str(i)
+    #     editor_interface.add_summ_line(session_id, last_minute, last_line_id, chars_before, chars_before + len(last_splits[0]) + 1)
 
 
 @app.route("/minuting/<session_id>", methods=["GET", "POST"])
@@ -114,7 +114,7 @@ def pad_change(pad_id):
             # we only need to react to transcript changes as reacting to summary changes would lead to infinite loops
             transcript = editor_interface.get_transcript(session_id)
             past_minutes = editor_interface.get_minutes(session_id)
-            update_minutes(session_id, transcript, past_minutes)
+            # update_minutes(session_id, transcript, past_minutes)
             # return ok response
             return jsonify({"status_code": 200, "message": "ok"})
         else:
---
+++ web_backend/transcription_worker/transcription_worker.py
@@ -0,0 +1,68 @@
+import pika
+import logging
+import audio_chunk
+import whisper_online
+
+from mosestokenizer import MosesTokenizer
+import os
+import time
+
+WHISPER_MODEL = "base.en"
+
+class MeetingTranscriber:
+    def __init__(self):
+        self.processors = {}
+
+    def add_chunk(self, track_id, chunk):
+        if track_id not in self.processors:
+            self.processors[track_id] = whisper_online.OnlineASRProcessor("en", backend, tokenizer)
+        self.processors[track_id].insert_audio_chunk(chunk)
+    
+    def get_new_utterances(self):
+        for track_id, processor in self.processors.items():
+            new_speech = processor.process_iter()
+            if new_speech[0] is None:
+                continue
+            print(f"{track_id}: {new_speech[2]}")
+
+    def is_ready(self):
+        return True
+
+class Transcripts:
+    def __init__(self):
+        self.transcripts = {}
+    
+    def _add_session(self, session_id):
+        self.transcripts[session_id] = MeetingTranscriber()
+    
+    def add_chunk(self, session_id, track_id, chunk):
+        # TODO: if multiprocessing becomes necessary, we will need locks here
+        if session_id not in self.transcripts:
+            self._add_session(session_id)
+        self.transcripts[session_id].add_chunk(track_id, chunk)
+        if self.transcripts[session_id].is_ready():
+            self.transcripts[session_id].get_new_utterances()
+
+
+def callback(ch, method, properties, body):
+    deserialized = audio_chunk.AudioChunk.deserialize(body)
+    transcripts.add_chunk(deserialized.session_id, deserialized.track_id, deserialized.chunk)
+    print(" [x] Received %r" % deserialized.session_id)
+
+if __name__ == "__main__":
+    # we now assume one worker, maybe we will scale to more later
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    if os.environ['MOCK_ML_MODELS'] == "true":
+        backend = whisper_online.FasterWhisperASR("en", modelsize=WHISPER_MODEL, device="cpu")
+    else:
+        backend = whisper_online.FasterWhisperASR("en", modelsize=WHISPER_MODEL, device="cuda")
+    tokenizer = MosesTokenizer("en")
+    print("Loaded ASR model")
+    transcripts = Transcripts()
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    channel = connection.channel()
+    channel.queue_declare("transcription_queue")
+    channel.basic_consume(queue='transcription_queue', on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
